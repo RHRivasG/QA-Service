abed475b5ca6d16bcecf2554480a5ea16d944d24
Ruth Rivas <rhrivasg@gmail.con>
2021-04-13 14:11:11 -0400

    create pages components

---
+++ qa-service-client/src/pages/QuestionList.svelte
@@ -0,0 +1,42 @@
+<script>
+	import QuestionCard from "../components/QuestionCard.svelte";
+	import QuestionInput from "../components/QuestionInput.svelte";
+	const questions = [
+		{
+			state: "accepted",
+			message: "What is Lorem Ipsum?",
+			user: "BMary89",
+			date: "11:09 PM - 12 Apr 2021",
+			answer:
+				"Lorem Ipsu is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+			dateAnswer: "11:05 PM - 13 Apr 2021",
+		},
+		{
+			state: "accepted",
+			message: "What is Lorem Ipsum?",
+			user: "BMary899",
+			date: "11:09 PM - 12 Apr 2021",
+			answer:
+				"Lorem Ipsu is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+			dateAnswer: "11:09 PM - 12 Apr 2021",
+		},
+	];
+</script>
+
+<style>
+	.content-layout {
+		padding-left: 10%;
+		padding-right: 10%;
+	}
+</style>
+
+<div class="content-layout">
+	<QuestionInput />
+
+	<div class="box has-text-left">
+		<h3 class="title is-3 has-text-info">Questions</h3>
+		{#each questions as question}
+			<QuestionCard {...question} />
+		{/each}
+	</div>
+</div>
