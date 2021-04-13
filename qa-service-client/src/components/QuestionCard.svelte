a762d6aff68852ddf00b4e21e6e81fdd71532387
Ruth Rivas <rhrivasg@gmail.con>
2021-04-13 14:12:00 -0400

    create questions components

---
+++ qa-service-client/src/components/QuestionCard.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let message;
+	export let user;
+	export let date;
+	export let answer;
+	export let dateAnswer;
+</script>
+
+<style>
+	.card {
+		border-style: solid;
+		border-color: #3298dc;
+		border-width: 1px;
+	}
+</style>
+
+<div class="card m-5">
+	<div class="card-content">
+		<div class="content">
+			<p class="title is-4 has-text-grey">{message}</p>
+			<p class="subtitle is-6 has-text-primary">{user} ({date})</p>
+			{answer}
+			<br />
+			<p class="has-text-right">
+				<time datetime="2021-4-12" class="has-text-right has-text-info">
+					{dateAnswer}
+				</time>
+			</p>
+		</div>
+	</div>
+</div>
---
+++ qa-service-client/src/components/QuestionInput.svelte
@@ -0,0 +1,40 @@
+<script>
+	let message = "";
+
+	function handleClick() {
+		console.log(message);
+		message = "";
+	}
+</script>
+
+<style>
+	.block {
+		display: block;
+		width: 100%;
+	}
+</style>
+
+<!--
+
+<div class="notification is-primary is-light">Question sent!</div>
+
+<div class="notification is-danger is-light">Send error.</div>
+-->
+
+<form class="box">
+	<label
+		for="question-input"
+		class="label is-size-4 has-text-grey has-text-left">
+		Do you have a question?</label>
+	<div class="field">
+		<p class="control is-expanded">
+			<textarea
+				bind:value={message}
+				class="textarea"
+				placeholder="e.g. Hello world" />
+		</p>
+	</div>
+	<button on:click={handleClick} class="button is-success block">
+		Send
+	</button>
+</form>
